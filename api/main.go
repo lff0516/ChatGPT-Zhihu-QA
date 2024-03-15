7e2bd77b0e380be33c7adcc4c408e71b7582b798
lff0516 <144296764+lff0516@users.noreply.github.com>
2024-03-15 18:17:45 +0800

    Add files via upload

---
+++ api/main.go
@@ -0,0 +1,23 @@
+package api
+
+import (
+	"net/http"
+	"qa/model"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Index 主页
+func Index(c *gin.Context) {
+	c.String(http.StatusOK, "======= https://github.com/Hui4401/qa =======")
+}
+
+// CurrentUser 获取当前用户
+func CurrentUser(c *gin.Context) *model.User {
+	if userID, _ := c.Get("user_id"); userID != nil {
+		if user, err := model.GetUser(userID); err == nil {
+			return &user
+		}
+	}
+	return nil
+}
